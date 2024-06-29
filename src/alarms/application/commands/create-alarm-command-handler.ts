f27f073c13fdc759b52a2ad206fe1adc87ce5234
Frank Mendez <frankmendezresources@gmail.com>
2024-06-29 12:43:51 +0800

    feat: Experimenting with CQRS. Part 2

--- src/alarms/application/commands/create-alarm-command-handler.ts
+++ src/alarms/application/commands/create-alarm-command-handler.ts
@@ -1,8 +1,9 @@
-import { CommandHandler, ICommandHandler } from '@nestjs/cqrs';
+import { CommandHandler, EventBus, ICommandHandler } from '@nestjs/cqrs';
 import { CreateAlarmCommand } from './create-command';
 import { AlarmRepository } from '../ports/alarm.repository';
 import { AlarmFactory } from '../../domain/factories/alarm.factory';
 import { Logger } from '@nestjs/common';
+import { AlarmCreatedEvent } from '../../domain/events/alarm-created.event';
 
 @CommandHandler(CreateAlarmCommand)
 export class CreateAlarmCommandHandler
@@ -12,11 +13,17 @@ export class CreateAlarmCommandHandler
   constructor(
     private readonly alarmRepository: AlarmRepository,
     private readonly alarmFactory: AlarmFactory,
+    private readonly eventBus: EventBus,
   ) {}
 
   async execute(command: CreateAlarmCommand) {
     this.logger.log(`CreateAlarmCommandHandler: ${JSON.stringify(command)}`);
     const alarm = this.alarmFactory.create(command.name, command.severity);
-    await this.alarmRepository.save(alarm);
+
+    // This is not yet the best way to dispatch events
+    // Domain events should be dispatched by the aggregate root, inside the domain layer,
+    // We'll cover this in the upcoming lessons
+    this.eventBus.publish(new AlarmCreatedEvent(alarm));
+    return alarm;
   }
 }
---
+++ src/alarms/application/event-handlers/alarm-created.event-handler.ts
@@ -0,0 +1,14 @@
+import { EventsHandler, IEventHandler } from '@nestjs/cqrs';
+import { AlarmCreatedEvent } from '../../domain/events/alarm-created.event';
+import { Logger } from '@nestjs/common';
+
+@EventsHandler(AlarmCreatedEvent)
+export class AlarmCreatedEventHandler
+  implements IEventHandler<AlarmCreatedEvent>
+{
+  private readonly logger = new Logger(AlarmCreatedEventHandler.name);
+
+  handle(event: AlarmCreatedEvent) {
+    this.logger.log(event);
+  }
+}
